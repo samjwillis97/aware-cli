c15e2eb47e722537768956c9c74a1263aa9c1eef
Sam Willis <sam@williscloud.org>
2022-09-20 12:48:59 +1000

    Fixed random int generator.

--- pkg/aware/helpers.go
+++ pkg/aware/helpers.go
@@ -15,11 +15,10 @@ func generateRandomFloat(min float64, max float64, decimals int) float64 {
 
 func generateRandomInt(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
-	return min + rand.Int()*(max-min)
+	return min + rand.Intn(max+1)
 }
 
 func generateRandomBool() bool {
-	// FIXME: Always False
 	return generateRandomInt(0, 1) == 1
 }
 
